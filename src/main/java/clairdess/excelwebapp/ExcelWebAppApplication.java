095809a58d3209ff574676257f02447450e8af90
Daniil Davydov <clairdess.github@gmail.com>
2022-05-12 15:16:17 +0900

    Initial commit

---
+++ src/main/java/clairdess/excelwebapp/ExcelWebAppApplication.java
@@ -0,0 +1,13 @@
+package clairdess.excelwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clairdess/excelwebapp/ExcelWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package clairdess.excelwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
